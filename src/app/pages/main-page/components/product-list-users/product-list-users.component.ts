7436613db5053a0c083b8b684a8723203c9829c2
Pereagrihorislav <leontiev272@gmail.com>
2023-11-02 21:55:45 +0300

    Добавлен просмотр объявлений пользователя

--- src/app/pages/main-page/components/product-list-users/product-list-users.component.ts
+++ src/app/pages/main-page/components/product-list-users/product-list-users.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { Product } from '../product/product.interface';
 
 @Component({
   selector: 'app-product-list-users',
   templateUrl: './product-list-users.component.html',
   styleUrls: ['./product-list-users.component.scss']
 })
-export class ProductListUsersComponent {
+export class ProductListUsersComponent implements OnInit {
+
+  userProducts: Array<Product> | undefined;
+
+  constructor (private userService: UserService ) {}
+
+  ngOnInit(): void {
+    this.userService.getCurrentUser().subscribe((response) => {
+      console.log(response);
+      this.userProducts = response.adverts
+      console.log(this.userProducts)
+    })
+  }
 
 }
