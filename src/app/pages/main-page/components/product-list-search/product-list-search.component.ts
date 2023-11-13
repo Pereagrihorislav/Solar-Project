6e62baf214251e9d5acc5c0152a23071721d074e
Pereagrihorislav <leontiev272@gmail.com>
2023-11-13 07:10:59 +0300

    Проведена ревизия механизма поиска

--- src/app/pages/main-page/components/product-list-search/product-list-search.component.ts
+++ src/app/pages/main-page/components/product-list-search/product-list-search.component.ts
@@ -2,8 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { EMPTY, Subscription, switchMap, tap } from 'rxjs';
 import { SearchService } from 'src/app/layout-module/services/search-service/search.service';
 import { Product } from '../../../interfaces/product.interface';
-import { Router } from '@angular/router';
-import { Category, CategoryShort } from 'src/app/layout-module/interfaces/categories.interface';
+import { CategoryShort } from 'src/app/layout-module/interfaces/categories.interface';
 
 @Component({
   selector: 'app-product-list-search',
@@ -13,48 +12,18 @@ import { Category, CategoryShort } from 'src/app/layout-module/interfaces/catego
 
 export class ProductListSearchComponent implements OnInit, OnDestroy {
   currentSearchInput!: CategoryShort;
-  //currentCategoryInput: Category = {id: '', name: ''};
+ 
   products: Array<Product> | undefined;
   refreshBySearchSub$!: Subscription;
   searchHotReloadSub$!: Subscription;
-  categorySearchHotReloadSub$!: Subscription;
   searchByInputSub$!: Subscription;
-  searchByCategorySub$!: Subscription;
   
-
- constructor(private searchService: SearchService, private router: Router){}
+ constructor(private searchService: SearchService){}
 
   displaySearchResults(): void {
-
     this.searchByInputSub$ = this.searchService.search(this.currentSearchInput).subscribe((response) => {
       this.products = response;
-      console.log(response + ' RESPONSE')
-      console.log(this.products)
     });
-    
-    /*if(this.currentSearchInput && !this.currentCategoryInput.id ) {
-      this.searchByInputSub$ = this.searchService.search(this.currentSearchInput).subscribe((response) => {
-        this.products = response;
-        console.log(this.products)
-      });
-    } else if (this.currentCategoryInput.id && !this.currentSearchInput) {
-      this.searchByCategorySub$ = this.searchService.searchByCategory(this.currentCategoryInput).subscribe((response) => {
-        this.products = response;
-        //this.searchService.changeSearchCategory('','');
-        this.currentCategoryInput.id = '';
-      });
-    } else if (this.currentSearchInput && this.currentCategoryInput) {
-        this.searchByCategorySub$ = this.searchService.searchByCategory(this.currentCategoryInput).subscribe((response) => {
-          this.products = response;
-          if(response.length === 0) {
-            this.searchByInputSub$ = this.searchService.search(this.currentSearchInput).subscribe((response) => {
-              this.products = response;
-            });
-          }
-        //this.searchService.changeSearchInput('');
-        this.currentCategoryInput = '';
-      });
-    }*/
   }
 
   ngOnInit(): void {
@@ -62,19 +31,8 @@ export class ProductListSearchComponent implements OnInit, OnDestroy {
       this.currentSearchInput = searchFormInput;
       const storedCategory = localStorage.getItem('last-search-input');
       this.currentSearchInput = storedCategory ? JSON.parse(storedCategory) : '';
-      console.log(this.currentSearchInput)
-
-
+  
     });
-
-    /*this.categorySearchHotReloadSub$ = this.searchService.searchCategory$.subscribe((searchCategory) => {
-      this.currentCategoryInput = searchCategory;
-      if (!searchCategory.id) {
-        const storedCategory = localStorage.getItem('last-category-input');
-        this.currentCategoryInput = storedCategory ? storedCategory.replace(/"/g, '') : '';
-      }
-    });*/
-    
 
    this.refreshBySearchSub$ = this.searchService.searchInput$
    .pipe(
@@ -83,21 +41,11 @@ export class ProductListSearchComponent implements OnInit, OnDestroy {
       return EMPTY;
     })
    ).subscribe();
-
-   /*this.searchService.searchCategory$
-   .pipe(
-    switchMap(() => {
-      this.displaySearchResults();
-      return EMPTY;
-    })
-   ).subscribe();*/
   }
 
   ngOnDestroy(): void {
   this.refreshBySearchSub$?.unsubscribe();
   this.searchHotReloadSub$?.unsubscribe();
-  this.categorySearchHotReloadSub$?.unsubscribe();
   this.searchByInputSub$?.unsubscribe();
-  this.searchByCategorySub$?.unsubscribe();
   }
 }
